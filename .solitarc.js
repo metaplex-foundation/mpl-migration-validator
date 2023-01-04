a2079c47a4f522e376ea36830cc24beac563ffaf
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2023-01-04 13:11:29 -0900

    fix solita path issue; bump version

--- .solitarc.js
+++ .solitarc.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const programDir = path.join(__dirname, 'program');
 const idlDir = path.join(__dirname, 'packages', 'sdk', 'idl');
-const sdkDir = path.join(__dirname, 'packages', 'sdk', 'src', 'generated');
+const sdkDir = path.join(__dirname, 'packages', 'sdk', 'src', 'generated', 'src');
 const binaryInstallDir = path.join(__dirname, '.crates');
 
 module.exports = {

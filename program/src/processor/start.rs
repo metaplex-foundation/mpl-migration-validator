8820ca18d2a963591bae2710392b3841b8c68fe9
Samuel Vanderwaal <samuel.vanderwaal@gmail.com>
2023-01-05 11:28:57 -0900

    add more start tests

--- program/src/processor/start.rs
+++ program/src/processor/start.rs
@@ -3,7 +3,6 @@ use mpl_token_metadata::state::CollectionAuthorityRecord;
 use super::*;
 
 pub fn start_migration(program_id: &Pubkey, accounts: &[AccountInfo]) -> ProgramResult {
-    // return Err(MigrationError::FeatureDisabled.into());
     // Fetch accounts
     let account_info_iter = &mut accounts.iter();
     let payer_info = next_account_info(account_info_iter)?;
--- program/tests/start.rs
+++ program/tests/start.rs
@@ -2,13 +2,16 @@
 pub mod utils;
 
 use mpl_migration_validator::error::MigrationError;
-use mpl_migration_validator::instruction::UpdateArgs;
+use mpl_migration_validator::instruction::{start, UpdateArgs};
+use mpl_migration_validator::state::ProgramSigner;
 use mpl_migration_validator::{instruction::InitializeArgs, state::UnlockMethod};
 use mpl_token_metadata::pda::find_collection_authority_account;
 use mpl_token_metadata::state::{CollectionAuthorityRecord, TokenMetadataAccount};
 use num_traits::FromPrimitive;
 use solana_program::pubkey::Pubkey;
 use solana_program_test::{tokio, BanksClientError};
+use solana_sdk::signature::Keypair;
+use solana_sdk::transaction::Transaction;
 use solana_sdk::{instruction::InstructionError, signer::Signer, transaction::TransactionError};
 use utils::*;
 
@@ -115,4 +118,115 @@ async fn start_migration() {
         migratorr.authority()
     );
     assert_eq!(delegate_record.bump, bump);
+}
+
+#[tokio::test]
+async fn wrong_authority_fails() {
+    let mut context = setup_context().await;
+
+    let fake_authority = Keypair::new();
+
+    // Create a default NFT to use as a collection.
+    let nft = NfTest::new();
+    nft.mint_default(&mut context, None).await.unwrap();
+
+    // Create our migration state manager.
+    let migratorr = Migratorr::new(nft.mint_pubkey());
+
+    // Set up our initialize args
+    let unlock_method = UnlockMethod::Timed;
+
+    let args = InitializeArgs {
+        rule_set: None, // this defaults to the default public key
+        unlock_method,
+        collection_size: 0,
+    };
+
+    let payer = context.payer.dirty_clone();
+
+    // Initialize the migration state account on-chain
+    let err = migratorr
+        .initialize(&mut context, &payer, &fake_authority, &nft, args)
+        .await
+        .unwrap_err();
+
+    assert_custom_error_ix!(0, err, MigrationError::InvalidAuthority);
+}
+
+#[tokio::test]
+async fn incorrect_migration_state_fails() {
+    let mut context = setup_context().await;
+
+    let other_authority = Keypair::new();
+    other_authority
+        .airdrop(&mut context, 1_000_000_000)
+        .await
+        .unwrap();
+
+    // Create a default NFT to use as a collection.
+    let nft = NfTest::new();
+    nft.mint_default(&mut context, None).await.unwrap();
+
+    // NFT for someone else's migration state account.
+    let other_nft = NfTest::new();
+    other_nft
+        .mint_default(&mut context, Some(&other_authority))
+        .await
+        .unwrap();
+
+    // Create our migration state managers.
+    let migratorr = Migratorr::new(nft.mint_pubkey());
+    let other_migratorr = Migratorr::new(other_nft.mint_pubkey());
+
+    // Set up our initialize args
+    let unlock_method = UnlockMethod::Timed;
+
+    let args = InitializeArgs {
+        rule_set: None, // this defaults to the default public key
+        unlock_method,
+        collection_size: 0,
+    };
+
+    let payer = context.payer.dirty_clone();
+
+    // Initialize both states.
+    migratorr
+        .initialize(&mut context, &payer, &payer, &nft, args.clone())
+        .await
+        .unwrap();
+
+    other_migratorr
+        .initialize(&mut context, &payer, &other_authority, &other_nft, args)
+        .await
+        .unwrap();
+
+    let delegate = ProgramSigner::pubkey();
+    let (delegate_record, _) = find_collection_authority_account(&nft.mint_pubkey(), &delegate);
+
+    println!("delegate_record: {:?}", delegate_record);
+
+    let instruction = start(
+        payer.pubkey(),
+        payer.pubkey(),
+        nft.mint_pubkey(),
+        nft.metadata_pubkey(),
+        delegate,
+        delegate_record,
+        other_migratorr.pubkey(),
+    );
+
+    let transaction = Transaction::new_signed_with_payer(
+        &[instruction],
+        Some(&payer.pubkey()),
+        &[&payer],
+        context.last_blockhash,
+    );
+
+    let err = context
+        .banks_client
+        .process_transaction(transaction)
+        .await
+        .unwrap_err();
+
+    assert_custom_error_ix!(0, err, MigrationError::InvalidStateDerivation);
 }
